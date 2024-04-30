8222ae0c2bb55a2552e27eacb7301df89f7b1038
camillaveloso <166400880+camillaveloso@users.noreply.github.com>
2024-04-29 21:50:38 -0300

    Update conversor_binario.py

--- conversor_binario.py
+++ conversor_binario.py
@@ -1,10 +1,10 @@
 import numpy as np
 for numero in [32, 57, 101, 77]:
         print(f'O número {numero} em código binário é: {np.binary_repr(numero)}')
-print("Deseja testar outros números? Se sim, digite o número desejado. Se não, digite 0 pra sair: ")
+print("Deseja testar outros números? Se sim, digite o número desejado. Se não, digite -1 pra sair: ")
 while True:
     continuar = int(input("Digite um número: "))
-    if continuar == 0:
+    if continuar == -1:
         break
     
     else:
