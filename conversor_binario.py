de06f9be540568b60efdd391d14ed4383abc74c1
camillaveloso <166400880+camillaveloso@users.noreply.github.com>
2024-04-29 21:29:12 -0300

    Update conversor_binario.py

--- conversor_binario.py
+++ conversor_binario.py
@@ -1,15 +1,3 @@
-import random as random
-
-for inteiro in [32, 57, 101, 77]:
-    binario = bin(inteiro)[2:]
-    print(f'O número {inteiro} em código binário é: {binario}')
-
-while True:
-    continuar = int(input("Deseja testar números aleatórios? Se sim, digite qualquer número. Se não, digite 0 pra sair: "))
-    if continuar == 0:
-        break
-    
-    else:
-        aleatorio = random.randint(0, 1000)
-        binario = bin(aleatorio)[2:]
-        print(f'O número {aleatorio} em código binário é: {binario}')
+import numpy as np
+for numero in [32, 57, 101, 77]:
+        print(f'O número {numero} em código binário é: {np.binary_repr(numero)}')
